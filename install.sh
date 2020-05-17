0989b57afd74eee4b40883329c7744c00778cee6
koent <ktbbb5@gmail.com>
2020-05-17 21:49:37 +0200

    kloten met install

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 
 HERE=`pwd`
 
-ln -s ${HERE}/i3/ ~/.config/i3
-ln -s ${HERE}/mutt/ ~/.mutt
-ln -s ${HERE}/i3blocks ~/.config/i3blocks
-ln -s ${HERE}/dunst ~/.config/dunst
-ln -s ${HERE}/rofi ~/.config/rofi
+ln -fs $(HERE)/i3 ~/.config/i3
+ln -fs $(HERE)/mutt ~/.mutt
+ln -fs $(HERE)/i3blocks ~/.config/i3blocks
+ln -fs $(HERE)/dunst ~/.config/dunst
+ln -fs $(HERE)/rofi ~/.config/rofi
 
