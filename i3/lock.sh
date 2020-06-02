effe35ec123f31656b78436b349aee240a0241aa
koent <ktbbb5@gmail.com>
2020-06-02 10:50:03 +0200

    Screenshots voor i3lock worden verwijderd (#8)

--- i3/lock.sh
+++ i3/lock.sh
@@ -2,3 +2,5 @@ scrot /tmp/screen.png
 #convert /tmp/screen.png -colorspace Gray -blur 0x5 /tmp/screen6.png
 convert /tmp/screen.png -colorspace Gray -scale 10% -scale 1000% /tmp/screen6.png
 i3lock -i /tmp/screen6.png
+rm /tmp/screen.png
+rm /tmp/screen6.png
