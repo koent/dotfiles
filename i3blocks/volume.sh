ff466c0c8647ecdc81ace9d366590609a9e7de52
koent <ktbbb5@gmail.com>
2020-06-06 19:02:33 +0200

    Volume wijzigen werkt (Zie ook #19)

--- i3blocks/volume.sh
+++ i3blocks/volume.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-out=`pactl list sinks | grep -P "\tVolume:" | grep -Po "\d+%"| head -1`
+out=`pactl list sinks | grep -P "\tVolume:" | tail -n 1 | grep -Po "\d+%"| head -1`
+#                                             XXXXXXX
+# Het is gewoon toeval dat de goede sink de laatste is
 
 echo $out
 echo $out
 
-if pactl list sinks | grep Mute | head -1 | grep ja > /dev/null
+if pactl list sinks | grep Mute | tail -n 1 | grep ja > /dev/null
 then
   echo "#aaaaaa"
 fi
